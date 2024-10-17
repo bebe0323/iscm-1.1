306f6023be00ae5145b1199ea2d5e0a7b9f7b09c
bebe0323 <belgutei0323@gmail.com>
2024-10-17 23:58:52 +1100

    removed unused ui of signin & signup

--- components/ui/Auth.tsx
+++ components/ui/Auth.tsx
@@ -1,37 +1,5 @@
-import { signup, signin, signout } from "@/app/actions/auth";
+import { signout } from "@/app/actions/auth";
 import { UserType } from "@/app/types/user";
-
-export function SignupForm() {
-  return (
-    <form action={signup}>
-      <div>
-        <label htmlFor="email">email</label>
-        <input id="email" name="email" placeholder="email" />  
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input id="password" name="password" type="password" />
-      </div>
-      <button type="submit">Sign Up</button>
-    </form>
-  )
-}
-
-export function SigninForm() {
-  return (
-    <form action={signin}>
-      <div>
-        <label htmlFor="email">email</label>
-        <input id="email" name="email" placeholder="email" />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input id="password" name="password" type="password" />
-      </div>
-      <button type="submit">Sign in</button>
-    </form>
-  )
-}
 
 export function Profile({user}: {user: UserType}) {
   return (
