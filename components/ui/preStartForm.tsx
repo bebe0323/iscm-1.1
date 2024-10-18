dce0ba68eb686a7c8d9b4a8f34038080ba080114
bebe0323 <belgutei0323@gmail.com>
2024-10-18 23:59:42 +1100

    added date picker for pre-start-talk

--- components/ui/preStartForm.tsx
+++ components/ui/preStartForm.tsx
@@ -1,20 +1,33 @@
 "use client";
 
+import * as React from "react"
 import { useState } from "react";
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+// imports for components
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
-import { Textarea } from "@/components/ui/textarea"
-import { Button } from "./button";
+} from "@/components/ui/select";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 
 export default function PreStartForm() {
   const [error, setError] = useState<boolean>(false);
   const [isLoading, setLoading] = useState<boolean>(false);
+  const [date, setDate] = React.useState<Date>()
 
   const handleSubmit = async (formData: FormData) => {
     setLoading(true);
@@ -30,7 +43,7 @@ export default function PreStartForm() {
       <div className="mt-5 px-4">
         <p>
           All Toolbox/Pre-start Talks undertaken on behalf of 
-          <i>ISCM Pry Ltd</i> are recorded on this form and 
+           <i>ISCM Pry Ltd</i> are recorded on this form and 
           signed by participants.
         </p>
       </div>
@@ -58,6 +71,31 @@ export default function PreStartForm() {
               <SelectItem value="system">System</SelectItem>
             </SelectContent>
           </Select>
+        </div>
+        <div className="flex">
+          <p>DATE</p>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                variant={"outline"}
+                className={cn(
+                  "w-[280px] justify-start text-left font-normal",
+                  !date && "text-muted-foreground"
+                )}
+              >
+                <CalendarIcon className="mr-2 h-4 w-4" />
+                {date ? format(date, "PPP") : <span>Pick a date</span>}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0">
+              <Calendar
+                mode="single"
+                selected={date}
+                onSelect={setDate}
+                initialFocus
+              />
+            </PopoverContent>
+          </Popover>
         </div>
         <div className="mt-8">
           <p className="bg-stone-200">DAILY WORK ACTIVITIES</p>
