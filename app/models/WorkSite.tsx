bbe61ffd288bb49416c8160e731dbead149c513d
bebe0323 <belgutei0323@gmail.com>
2024-10-21 11:52:18 +1100

    added worksite schema

---
+++ app/models/WorkSite.tsx
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const workSiteSchema = new Schema({
+  created_by: { // _id of the person who created it
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startedAt: { // the date the job started
+    type: Date,
+    default: null,
+  },
+  endedAt: { // the date the job ended
+    type: Date,
+    default: null,
+  },
+  status: {
+    // 0 - not started
+    // 1 - in progress
+    // 2 - finished
+    type: Number,
+    default: 0,
+    min: 0,
+    max: 2,
+  },
+});
+
+export const WorkStieModel = mongoose.models.WorkSite ||
+                             mongoose.model("WorkSite", workSiteSchema);
