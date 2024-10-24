0aa3233a2e53c891b83400c9ab52dc6673e8ef84
bebe0323 <belgutei0323@gmail.com>
2024-10-24 20:40:03 +1100

    added custom table for worksites

--- app/worksites/page.tsx
+++ app/worksites/page.tsx
@@ -1,14 +1,16 @@
 
+import { WorkSiteTable } from "@/components/worksite/workSiteTable";
 import { getWorkSites } from "../actions/workSite";
 
 import { DataTableNoSelection } from "../users/data-table";
 import { workSiteColumns } from "./workSiteColumns";
 
 export default async function Page() {
-  const data = await getWorkSites({ status: 0 });
+  const data = await getWorkSites({ status: -1 });
   return (
     <div className="container mx-auto py-10">
       <DataTableNoSelection columns={workSiteColumns} data={data} />
+      <WorkSiteTable workSites={data} />
     </div>
   )
 }
---
+++ components/worksite/workSiteTable.tsx
@@ -0,0 +1,38 @@
+import { TypeWorkSiteClient } from "@/app/types/workSite";
+
+function formatDate(date: Date | null) {
+  if (!date) {
+    return "Not date";
+  }
+  const formattedDate = date.toLocaleDateString('en-AU', { 
+    day: '2-digit', 
+    month: 'long', // Use 'long' for full month name
+    year: 'numeric'
+  });
+  return formattedDate;
+}
+
+export function WorkSiteTable({
+  workSites
+}: {
+  workSites: TypeWorkSiteClient[]
+}) {
+  return (
+    <div className=" mt-10 border rounded-md">
+      <div className="flex w-full px-2 text-zinc-500 text-sm py-2">
+        <div className="w-1/4">Status</div>
+        <div className="w-1/4">Address</div>
+        <div className="w-1/4">Started at</div>
+        <div className="w-1/4">Ended at</div>
+      </div>
+      {workSites.map((workSite) => (
+        <div key={workSite._id} className="border-t flex w-full px-2 py-1.5">
+          <div className="w-1/4">{workSite.status}</div>
+          <div className="w-1/4">{workSite.address}</div>
+          <div className="w-1/4">{formatDate(workSite.startedAt)}</div>
+          <div className="w-1/4">{formatDate(workSite.endedAt)}</div>
+        </div>
+      ))}
+    </div>
+  )
+}
