957cc90e3a7fb3cb2386bef476f328b794e41162
Belgutei <z5458085@ad.unsw.edu.au>
2024-11-08 12:10:45 +1100

    removed worksites table from pre-built componenet

--- app/worksites/page.tsx
+++ app/worksites/page.tsx
@@ -2,14 +2,10 @@
 import { WorkSiteTable } from "@/components/worksite/workSiteTable";
 import { getWorkSites } from "../actions/workSite";
 
-import { DataTableNoSelection } from "../users/data-table";
-import { workSiteColumns } from "./workSiteColumns";
-
 export default async function Page() {
   const data = await getWorkSites({ status: -1 });
   return (
     <div className="container mx-auto py-10">
-      <DataTableNoSelection columns={workSiteColumns} data={data} />
       <div className="mx-5">
         <WorkSiteTable workSites={data} />
       </div>
